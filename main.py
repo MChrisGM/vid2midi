57bbbc54b298ab24521a7b43c36bf667ec18beba
MChrisGM <c.i.cristakos@gmail.com>
2022-11-29 07:05:40 +0000

    Sort-of key registering

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import numpy as np
 import cv2
-from PIL import Image
+from PIL import Image, ImageOps
+import math
 
-INTRO_SKIP = 60
+INTRO_SKIP = 0
 READ_ROW = 0
 BPM = 100
 FPS = 30
-KEYBOARD_KEYS = 28
+KEYBOARD_KEYS = 88
+
+LEFT_INDEX = 0
 
 VIDEO_PATH = 'IO/video.mp4'
 
@@ -17,13 +20,44 @@ if __name__=="__main__":
     while cap.isOpened():
         if count>INTRO_SKIP:
             ret,frame = cap.read()
-            # print(count,frame[0,0])
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            im_pil = Image.fromarray(frame)
-            im_pil = im_pil.convert('L')
-            cv2.imshow('window-name', np.asarray(im_pil))
-            
 
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            im_pil = Image.fromarray(frame)
+            im_pil = ImageOps.invert(im_pil.convert('L'))
+
+            fr = np.asarray(im_pil)
+            r1 = fr[0]
+
+            L = len(r1)
+            D = math.ceil((L/KEYBOARD_KEYS))
+
+            for i in range(len(fr[0])):
+                for j in range(1):
+                    # fr[j][i] = round(fr[j][i]/255)*255
+                    fr[j][i] = 0 if fr[j][i]<180 else 255
+            
+            for i in range(KEYBOARD_KEYS-1):
+                idx = LEFT_INDEX+round(D/2)+round(i*D)
+                for r in range(20,fr.shape[0]):
+                    fr[r][idx] = 0
+            
+            for i in range(KEYBOARD_KEYS-1):
+                idx = LEFT_INDEX+round(D/2)+round(i*D)
+                if fr[0][idx] == 0:
+                    if (fr[0][idx - round(D/3)] == 0) and (fr[0][idx + round(D/3)] == 0):
+                        for r in range(500,fr.shape[0]):
+                            for k in range(-5,5):
+                                fr[r][idx+k] = 0
+                
+            fr[0] = r1
+
+            # fr = cv2.cvtColor(fr,cv2.COLOR_GRAY2RGB)
+
+            cv2.imshow('window-name', fr)
+            
+        else:
+            ret = cap.read()
         # cv2.imshow('window-name', frame)
         # cv2.imwrite("frame%d.jpg" % count, frame)
         count +=1
